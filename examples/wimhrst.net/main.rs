143f2658e1d3737eef39593e953aa20af17e810b
oclyke <oclyke@oclyke.dev>
2024-01-24 04:13:50 -0800

    release v0.1.1

---
+++ examples/wimhrst.net/main.rs
@@ -0,0 +1,123 @@
+// site-content structure:
+// site-content/
+// |-  index.html
+// |-  style.css
+// |-  assets/
+// |    |-  * images *
+
+use std::cell::RefCell;
+use std::path::{Path, PathBuf};
+use std::rc::Rc;
+
+use wimhrst::{
+    builder::{Build, Builder, Dependency},
+    resource::{CopyFile, Root},
+};
+
+fn main() {
+    run().expect("Expected to exit successfully");
+}
+
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    println!("program start");
+
+    let example_dir = PathBuf::from("examples/basic");
+    let source = example_dir.join("site-content");
+    let output = example_dir.join("site-out");
+    let mut builder = Builder::new();
+    builder.init()?;
+
+    // clear the output directory
+    // prefer immutability over performance
+    // the directory tree will be assembled to replace the output directory
+    print!("removing output directory... ");
+    if output.exists() {
+        std::fs::remove_dir_all(&output)?;
+    }
+    println!("done.");
+
+    // register copied files
+    builder.require(CopyDir::new(source.join("assets"), output.join("assets")))?;
+    builder.require(CopyFile::new(
+        source.join("index.html"),
+        output.join("index.html"),
+    ))?;
+    builder.require(CopyFile::new(
+        source.join("style.css"),
+        output.join("style.css"),
+    ))?;
+    builder.require(CopyFile::new(
+        source.join("reset.css"),
+        output.join("reset.css"),
+    ))?;
+
+    builder.generate()?;
+
+    println!("program end");
+    Ok(())
+}
+
+#[derive(Debug)]
+struct CopyDir {
+    source: PathBuf,
+    path: PathBuf,
+}
+
+impl CopyDir {
+    fn new<P: AsRef<Path>>(source: P, path: P) -> Self {
+        Self {
+            source: source.as_ref().to_path_buf(),
+            path: path.as_ref().to_path_buf(),
+        }
+    }
+}
+
+impl PartialEq for CopyDir {
+    fn eq(&self, other: &Self) -> bool {
+        self.path == other.path
+    }
+}
+
+impl Build for CopyDir {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+    fn equals(&self, other: Rc<RefCell<dyn Build>>) -> bool {
+        let other = other.borrow();
+        let any = other.as_any();
+        match any.downcast_ref::<Self>() {
+            Some(other) => self == other,
+            None => false,
+        }
+    }
+    fn register(
+        self,
+        builder: &mut Builder,
+    ) -> Result<(Option<PathBuf>, Dependency, Vec<Dependency>), Box<dyn std::error::Error>> {
+        let path = self.path.clone();
+        let dependency = builder.make_dependency(self)?;
+        let root = builder.require(Root {})?;
+        Ok((Some(path), dependency, vec![root]))
+    }
+    fn generate(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let CopyDir { source, .. } = self;
+        if !source.is_dir() {
+            return Err("source is not a directory".into());
+        }
+        for entry in walkdir::WalkDir::new(&source) {
+            let entry = entry?;
+            let path = entry.path().to_path_buf();
+            let relpath = path.strip_prefix(&source)?.to_path_buf();
+            let output = self.path.join(relpath);
+            if path.is_file() {
+                let parent = match output.parent() {
+                    Some(parent) => parent,
+                    None => return Err("path has no parent".into()),
+                };
+                std::fs::create_dir_all(parent)?;
+                std::fs::copy(path, output)?;
+            }
+        }
+        Ok(())
+    }
+}
